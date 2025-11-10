c94903d4286dfa8cfa340be4b7f78d5f10e3194c
schelleg <graham.schelle@gmail.com>
2025-11-10 16:09:16 -0700

    s

---
+++ packages/workshops/sps2025/demo/setup_env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# Check if SPSDEMO_VENV or SPSDEMO_PATH is unset or empty
+if [[ -z "$SPSDEMO_VENV" || -z "$SPSDEMO_PATH" ]]; then
+    echo "Error: SPSDEMO_VENV and SPSDEMO_PATH must be set."
+    exit 1
+fi
+
+# Setup xdna driver (only needed one time)
+# For NPU to work secure boot needs to be disabled in BIOS
+pushd $SPSDEMO_PATH
+./Ryzers/packages/npu/xdna/install_xdna.sh
+
+popd
