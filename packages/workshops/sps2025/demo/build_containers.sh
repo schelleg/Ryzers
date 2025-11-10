eaed877b6f2a9384f7e4aa13ff71c6a0b81469c4
schelleg <graham.schelle@gmail.com>
2025-11-10 16:10:17 -0700

    s

---
+++ packages/workshops/sps2025/demo/build_containers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check if SPSDEMO_VENV or SPSDEMO_PATH is unset or empty
+if [[ -z "$SPSDEMO_VENV" || -z "$SPSDEMO_PATH" ]]; then
+    echo "Error: SPSDEMO_VENV and SPSDEMO_PATH must be set."
+    exit 1
+    
+
+source $SPSDEMO_VENV/bin/activate
+
+ryzers build genesis --name genesis
+ryzers build llamacpp smolvlm --name smolvlm
+ryzers build xdna ryzenai_cvml ros --name cvml
